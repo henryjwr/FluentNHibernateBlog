6b6ef5b5083658c90bb60f42b1d9dda9179776ce
Daniel Houk <daniel@ninecollective.com>
2011-08-30 23:09:49 -0500

    Fixed a problem with connectiing to the database and now it connects to the database perfectly.

---
+++ MemeCreator/Classes/Meme.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MemeCreator.Classes
+{
+    public class Meme
+    {
+        public virtual Guid Id { get; set; }
+        public virtual string ImagePath { get; set; }
+        public virtual string TopMemeLine { get; set; }
+        public virtual string BottomMemeLine { get; set; }
+    }
+}
--- MemeCreator/Program.cs
+++ MemeCreator/Program.cs
@@ -1,9 +1,35 @@
-﻿namespace MemeCreator
+﻿using System;
+using System.IO;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+
+namespace MemeCreator
 {
     class Program
     {
+        private static Configuration _configuration;
+        private const string DbFile = "memedatabase.db";
+        
         static void Main(string[] args)
         {
+            var factory = CreateSessionFactory();
+            var session = factory.OpenSession();
+            Console.Write("Done");
+            Console.Read();
+
+        }
+
+        private static ISessionFactory CreateSessionFactory()
+        {
+            var testing = Fluently
+                .Configure().Database(SQLiteConfiguration.Standard.InMemory())
+                .Mappings(m => m.FluentMappings.AddFromAssemblyOf<Program>())
+                .ExposeConfiguration(cfg => _configuration = cfg)
+                .BuildSessionFactory();
+            return testing;
         }
     }
 }
